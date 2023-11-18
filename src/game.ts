888d5f152c122d5dc335bdba50805c9bd0ca7838
Mark Butterworth <nokternol@users.noreply.github.com>
2023-11-18 13:43:03 +0000

    implement spare logic

--- src/game.ts
+++ src/game.ts
@@ -1,9 +1,19 @@
 export default class Game {
-  private _score = 0;
+  private scores = Array(21).fill(0);
+  private currRoll = 0;
+  private spare = false;
   score() {
-    return this._score;
+    return this.scores.reduce((acc, curr) => acc+curr, 0);
   }
   roll(pins: number) {
-    this._score += pins;
+    this.scores[++this.currRoll] = pins;
+    if (this.spare) {
+        this.scores[this.currRoll-1] += pins;
+        this.spare = false;
+    }
+    if (this.currRoll % 2 === 0 && this.scores[this.currRoll-1] + pins === 10) {
+        this.spare = true;
+    }
+
   }
 }

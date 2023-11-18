3789974fb5c934250a4302ce27ccdab6783b3592
Mark Butterworth <nokternol@users.noreply.github.com>
2023-11-18 13:15:30 +0000

    add first test

---
+++ tests/bowlingGame.test.ts
@@ -0,0 +1,7 @@
+import { describe, it } from "vitest";
+import Game from "../src/game";
+describe("bowling game tests", () => {
+  it("gutter game", () => {
+    const game = new Game();
+  });
+});

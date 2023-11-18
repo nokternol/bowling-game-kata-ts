2d76c837a81ba5fc7b081f6f125654f346a15106
Mark Butterworth <nokternol@users.noreply.github.com>
2023-11-18 13:28:27 +0000

    test setup

--- tests/bowlingGame.test.ts
+++ tests/bowlingGame.test.ts
@@ -1,13 +1,17 @@
-import { describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it } from "vitest";
 import Game from "../src/game";
 describe("bowling game tests", () => {
+  let game: Game;
+  beforeEach(() => {
+    game = new Game();
+  });
+
   it("gutter game", () => {
-    const game = new Game();
     for (let i = 0; i < 20; i++) game.roll(0);
     expect(game.score()).to.equal(0);
   });
+  
   it("single pin game", () => {
-    const game = new Game();
     for (let i = 0; i < 20; i++) game.roll(1);
     expect(game.score()).to.equal(20);
   });

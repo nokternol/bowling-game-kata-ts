ffd16a49ee85a2b9589fd7dc77cc705373a8e4c3
Mark Butterworth <nokternol@users.noreply.github.com>
2023-11-18 13:52:37 +0000

    move spare logic to calculated score

--- src/game.ts
+++ src/game.ts
@@ -1,19 +1,20 @@
 export default class Game {
   private scores = Array(21).fill(0);
   private currRoll = 0;
-  private spare = false;
   score() {
-    return this.scores.reduce((acc, curr) => acc+curr, 0);
+    return this.scores.reduce((acc, curr, i, arr) => {
+        const frameEnd = i % 2 === 0;
+        let score:  number;
+        if (frameEnd) {
+            const isSpare = curr + arr[i-1] === 10;
+            score = isSpare ? curr + arr[i+1] : curr;
+        } else {
+            score = curr;
+        }
+        return acc+score;
+    }, 0);
   }
   roll(pins: number) {
     this.scores[++this.currRoll] = pins;
-    if (this.spare) {
-        this.scores[this.currRoll-1] += pins;
-        this.spare = false;
-    }
-    if (this.currRoll % 2 === 0 && this.scores[this.currRoll-1] + pins === 10) {
-        this.spare = true;
-    }
-
   }
 }
